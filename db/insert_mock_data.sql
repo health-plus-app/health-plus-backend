fa4588295ce54cc153c4a5ce5cf0f72a94911fb4
Kevin James Hilton <kjhilton01@gmail.com>
2023-01-26 18:07:57 +0100

    move inserts into mock_data folder and run sql file for inserts

---
+++ db/insert_mock_data.sql
@@ -0,0 +1,19 @@
+-- uncomment if you want to reinitialize database
+-- DROP TABLE rates;
+-- DROP TABLE recommended_meals;
+-- DROP TABLE user_meals;
+-- DROP TABLE user_health_statistics;
+-- DROP TABLE contains;
+-- DROP TABLE ingredients;
+-- DROP TABLE meals;
+-- DROP TABLE users;
+-- \i schema.sql
+
+\i mock_data/users.sql
+\i mock_data/meals.sql
+\i mock_data/ingredients.sql
+\i mock_data/contains.sql
+\i mock_data/user_health_statistics.sql
+\i mock_data/user_meals.sql
+\i mock_data/recommended_meals.sql
+\i mock_data/rates.sql
