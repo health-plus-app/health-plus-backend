b68762de9c929de30abea7040f3dd6f565c4bb54
ericluuv <ericluuv@gmail.com>
2023-02-21 07:56:14 -0800

    Scraped ~ 33,000 recipes.

--- scraper/main.py
+++ scraper/main.py
@@ -3,33 +3,15 @@ import urllib.request
 import time
 import json
 from bs4 import BeautifulSoup
-# from nltk.tokenize import WhitespaceTokenizer
-# from nltk.corpus import stopwords
+from nltk.tokenize import WhitespaceTokenizer
+from nltk.corpus import stopwords
 from collections import defaultdict, deque
 from string import punctuation
-
-# stop_words = set(stopwords.words("english"))
-# tokenizer = WhitespaceTokenizer()
-# from simhash import Simhash, SimhashIndex
 
 frontier = deque()
 frontier.append("https://www.allrecipes.com/recipes/")
 # will go BFS style, neighbors first
 processed = set()
-
-
-# def check_dups(content):
-#     s = Simhash(get_features(content))
-#     # Outside code used below:
-#     # https://leons.im/posts/a-python-implementation-of-simhash-algorithm/
-#     objs = [(str(k), Simhash(get_features(v)))
-#             for k, v in simhash_data.data.items()]
-#     index = SimhashIndex(objs, k=3)
-#     if len(index.get_near_dups(s)) == 0:
-#         simhash_data.data[simhash_data.uniqueID] = content
-#         simhash_data.uniqueID += 1
-#         return False
-#     return True
 
 def build_processed():
     # Will parse the recipes.json file previously created and add all the URLs to the set
@@ -43,61 +25,69 @@ def build_processed():
     except Exception:
         pass # In case recipes.json() hasn't been made yet
 
-def get_tokens(text) -> [str]:
-    # Grabs text and removes stopwords, punctuation, etc.
-    to_return = []
-    temp = text.split('\n')
-    pot_tokens = tokenizer.tokenize(text)
-    for token in pot_tokens:
-        if token not in punctuation and token not in stop_words and len(token) > 1:
-            to_return.append(token)
-    return to_return
-
 def get_nutrition_table(soup) -> dict:
     # Grabs nutritional values from html table
+    mappings = {
+        'servings per recipe': 'servings',
+        'total carbohydrate': 'carbohydrate',
+        'dietary fiber': 'fiber',
+        'total sugars': 'sugar',
+    }
     to_return = {}
     rows = soup.find_all("table", {"class": "mntl-nutrition-facts-label__table"})
     for row in rows:
         for col in row.findAll(['td', 'tr']):
             temp = col.text.strip().split('\n')
             if len(temp) > 1:
-                to_return[temp[0]] = temp[1]
+                lowered = temp[0].lower()
+                new_key = mappings.get(lowered, lowered).replace(' ', '_')
+                to_return[new_key] = temp[1]
     return to_return
 
 
 def get_nutrition_script(soup) -> dict:
     # Grabs ingredients from application/ld+json
-    keys = ["name", "recipeIngredient", "recipeInstructions"]
     to_return = {}
     try:
         contents = soup.find("script", {"type":"application/ld+json"}).contents[0].strip()
         info = json.loads("".join(contents)[1:-1])
-        for key in keys:
-            to_return[key] = info.get(key)
+        to_return["ingredients"] = info.get("recipeIngredient", "")
+        to_return["name"] = info.get("name", "")
+        to_return["instructions"] = []
+        for obj in info.get("recipeInstructions", []):
+            to_return["instructions"].append(obj.get("text"))
     except Exception as e:
         print(f"Error getting javascript: {e}") # Skip parsing exceptions
     return to_return
 
+def get_food_info(soup) -> dict:
+    # Parses all information from the HTML
+    food_info = get_nutrition_table(soup)
+    food_info.update(get_nutrition_script(soup))
+    for elem in soup.find_all("meta"):
+        prop = elem.get("property", "")
+        if prop == "og:image":
+            food_info["image_url"] = elem.get("content", "")
+        elif prop == "og:description":
+            food_info["description"] = elem.get("content", "")
+    # Rename keys to fit conventions
+    return food_info
 
 
 def extract_next_links(soup):
     to_return = set()
     for link in soup.find_all('a'):
         pot_link = link.get('href', None)
-        if pot_link and 'https://www.allrecipes.com/recipes' in pot_link and pot_link:
+        if pot_link and 'https://www.allrecipes.com/recipes' in pot_link:
             to_return.add(pot_link)
-    contents = soup.find("script", {"type":"application/ld+json"}).contents[0].strip()
-    info = json.loads("".join(contents)[1:-1])
     # Recipe pages and pages for recipes have different structures
     try:
+        contents = soup.find("script", {"type":"application/ld+json"}).contents[0].strip()
+        info = json.loads("".join(contents)[1:-1])
         recipes = info.get("mainEntityOfPage").get("breadcrumb").get("itemListElement")
         for recipe in recipes:
-            url = recipe.get('item').get('@id')
             to_return.add(recipe.get('item').get('@id'))
-    except Exception:
-        pass
-    try:
-        for item in info.get("itemListElement"):
+        for item in info.get("itemListElement", []):
             url = item.get("url")
             to_return.add(url)
     except Exception:
@@ -107,27 +97,25 @@ def extract_next_links(soup):
 def run():
     counter = 0
     build_processed()
-    while (frontier):
+    while frontier:
         url_to_search = frontier.pop()
         time.sleep(1) # Politeness delay
         html_text = requests.get(url_to_search).content
         soup = BeautifulSoup(html_text, features="lxml", from_encoding='utf8')
         # Get info on the food
-        food_info = get_nutrition_table(soup)
-        food_info.update(get_nutrition_script(soup))
+        food_info = get_food_info(soup)
         food_info['url'] = url_to_search
         for k, v in food_info.items():
             print(f"{k}: {v}")
-        if food_info.get("name") and food_info.get("recipeIngredient"):
+        if food_info.get("name") and food_info.get("ingredients"):
             # Means not an article page, append food info to a file
             with open("recipes.json", "a") as file:
-                file.write(json.dumps(food_info))
+                file.write(json.dumps(food_info)+"\n")
         processed.add(url_to_search)
         # Get next links from the HTML
-        pots = extract_next_links(soup)
+        pots = extract_next_links(soup).difference(processed)
         for pot in pots:
-            if pot not in processed:
-                frontier.append(pot)
+            frontier.append(pot)
         counter += 1
         
 
