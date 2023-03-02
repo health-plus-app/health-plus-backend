4af31d4475de1bb2807bd0c1af41eefbc7473006
Kevin James Hilton <kjhilton01@gmail.com>
2023-03-02 15:29:43 -0800

    format user router and merge user profiles post and put

--- routes/user.router.js
+++ routes/user.router.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
+const uuid = require('uuid');
 
 const Pool = require('pg').Pool
 const pool = new Pool({
@@ -13,141 +14,107 @@ const pool = new Pool({
 const router = express.Router();
 
 // Login
-router.post('/login', async(req,res) => {
-    console.log(req.body)
-    const {email, password} = req.body
-    pool.query('select * from users where email=$1', [email], (error, results) => {
-        if (error) {
-            throw error
-        }
-        if (results.rowCount == 0) {
+router.post('/login', async (req,res) => {
+    const {email, password} = req.body;
+    const results = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
+    
+    if (results.rowCount === 0) {
+        res.status(401).json({
+            message: "Login not successful",
+            error: "User not found",
+        })
+    } else {
+        const password_matches = await bcrypt.compare(password, results.rows[0].password);
+        if (password_matches) {
+            res.status(200).json({
+                message: "Login successful",
+            })    
+        } else {
             res.status(401).json({
-                message: "Login not successful",
-                error: "User not found",
+                message: "Password is invalid",
             })
         }
-        else {
-            bcrypt.compare(password, results.rows[0].password).then(function(result) {
-                result ?
-                res.status(200).json({
-                    message: "Login successful",
-                    results
-                })
-                : res.status(401).json({
-                    message: "Login not successful",
-                })
-
-            })
-        }
-    })
+    }
 })
 
 // Register a user
-router.post('/register', async(req, res) => {
-    console.log(req.body);
-    const {id, email, password} = req.body
+router.post('/register', async (req, res) => {
+    const { email, password } = req.body
 
     const hashedPassword = await bcrypt.hash(password, 10)
+    const user_id = uuid.v4();
 
-    pool.query('insert into users (id, email, password) values($1,$2,$3)', [id, email, hashedPassword], (error, results) => {
-        if (error) {
-            throw error
-        }
-        res.status(200).json({
-            message: "User successfully created",
-
-        })
+    await pool.query('INSERT INTO USERS (id, email, password) VALUES($1,$2,$3)', [user_id, email, hashedPassword]);
+    res.status(200).json({
+        message: "User successfully created",
+        id: user_id
     })
 })
 
 // Get User Profile
-router.get('/info/:id', async(req,res) => {
+router.get('/info/:id', async (req,res) => {
     const { id } = req.params;
-    pool.query('SELECT * FROM user_profiles where user_id = $1', [id], (error, results) => {
-        if (error) {
-            throw error
-        }
-        if (results.rowCount == 0){
-            res.status(401).json({
-                message: "Could not find user profile",
-                error: "User profile not found",
-            })
-        }
-        res.status(200).json(results.rows)
-    })
+
+    const results = await pool.query('SELECT * FROM user_profiles WHERE user_id = $1', [id]);
+    if (results.rowCount === 0){
+        res.status(404).json({
+            message: "User profile not found",
+            error: "No user exists with specified user id"
+        })
+    }
+    res.status(200).json(results.rows);
 })
 
-// Update User Profile
-router.put('/info/:id', async(req,res) => {
-    console.log(req.body)
-    const {id} = req.params;
+// helper function to test if user exists
+const user_exists = async id => {
+    const res = await pool.query('SELECT * FROM users WHERE id = $1', [id]);
+    if (res.rowCount === 0) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+// Put User Profile
+router.put('/info/:id', async (req,res) => {
+    const { id } = req.params;
     const { goal, weight, allergies } = req.body;
-    pool.query('update user_profiles set fitness_goal = $1, weight=$2, allergies=$3 where user_id = $4', [goal, weight, allergies, id], (error, results) => {
-        if (error) {
-            throw error
-        }
-        if (results.rowCount == 0){
-            res.status(401).json({
-                message: "Could not find user profile",
-                error: "User profile not found",
-            })
-        }
-        res.status(200).json(results.rows)
-    })
-})
 
+    if (!(await user_exists(id))) {
+        res.status(404).json({
+            message: "Could not find user profile",
+            error: "No user exists with specified user id"
+        });
+        return;
+    }
 
-// Post User Profile
-router.post('/info', async(req,res) => {
-    const { id,  goal, weight, allergies} = req.body;
-    pool.query('insert into user_profiles (user_id, fitness_goal, weight, allergies) values($1,$2,$3, $4)', [id, goal, weight, allergies], (error, results) => {
-        if (error) {
-            throw error
-        }
-        if (results.rowCount == 0){
-            res.status(401).json({
-                message: "Could not create profile",
-                error: "User profile not created",
-            })
-        }
-        res.status(200).json(results.rows)
-    })
-})
-
-// Get User Health Statistics
-router.get('/health/:id', async(req,res) => {
-    const { id, data } = req.params;
-    pool.query('SELECT * FROM user_health_statistics where id = $1 and date_recorded = $2', [id, data], (error, results) => {
-        if (error) {
-            throw error
-        }
-        if (results.rowCount == 0){
-            res.status(401).json({
-                message: "Could not find user profile",
-                error: "User profile not found",
-            })
-        }
-        res.status(200).json(results.rows)
-    })
+    const results = (await pool.query('SELECT * FROM user_profiles WHERE user_id = $1', [id]));
+    if (results.rowCount === 0) {
+        await pool.query('INSERT INTO user_profiles (user_id, fitness_goal, weight, allergies) VALUES($1,$2,$3,$4)', [id, goal, weight, allergies]);
+        res.status(200).json({
+            message: "User Profile successfully created",
+        });
+    } else {
+        await pool.query('UPDATE user_profiles SET fitness_goal = $1, weight = $2, allergies = $3 WHERE user_id = $4', [goal, weight, allergies, id]);
+        res.status(200).json({
+            message: "User Profile successfully updated",
+        });
+    }
 })
 
 // Get user by id
 router.get('/:id', async (req,res) => {
     const { id } = req.params;
-    pool.query('SELECT * FROM users where id = $1', [id], (error, results) => {
-        if (error) {
-            throw error
-        }
-        if (results.rowCount == 0){
-            res.status(401).json({
-                message: "Could not find user",
-                error: "User not found",
-            })
-        }
+    const results = await pool.query('SELECT * FROM users WHERE id = $1', [id]);
+
+    if (results.rowCount === 0){
+        res.status(401).json({
+            message: "Could not find user",
+            error: "No user exists with specified user id",
+        })
+    } else {
         res.status(200).json(results.rows)
-    })
+    }
 })
-
-
 
 module.exports = router;
